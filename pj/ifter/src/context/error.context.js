325cef2d1b1d26b3231f41663a8d48492975eba7
Gabriel De Moraes Paiz <gabriel.moraes@cwi.com.br>
2022-07-16 17:21:04 -0300

    retirando a pasta pj

--- pj/ifter/src/context/error.context.js
+++
@@ -1,9 +0,0 @@
-import createGlobalState from "react-create-global-state";
-
-const stringifyError = localStorage.getItem("error");
-
-const error = JSON.parse(stringifyError) || {};
-
-const [useGlobalError, ErrorGlobalProvider] = createGlobalState(error);
-
-export { useGlobalError, ErrorGlobalProvider };
--- pj/ifter/src/hooks/api/_base/use-axios.hook.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export function useAxios(baseUrl, headers) {
-  return axios.create({
-    baseURL: baseUrl,
-    headers,
-  });
-}
